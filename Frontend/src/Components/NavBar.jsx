66f91364cc497e7e8f9889e90c09c4d03dc163e5
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-14 00:59:29 +0530

    minor fix

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <header className='bg-[#009bd5]/20 sticky top-0 z-10 shadow-md'>
             <nav className="flex items-center justify-between h-28 max-w-7xl mx-auto px-6">
-                <a href="#"><img src={logo} alt="ACM Thapar Logo" className="h-[102px] w-auto" /></a>
+                <a href="/"><img src={logo} alt="ACM Thapar Logo" className="h-[102px] w-auto" /></a>
                 <div className="flex items-center gap-4 h-14 w-32">
                     <a href="#"><img src={InstagramIcon} alt="Instagram" className="h-full" /></a>
                     <a href="#"><img src={GitHubIcon} alt="GitHub" className="h-full" /></a>
