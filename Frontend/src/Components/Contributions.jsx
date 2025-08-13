fd7fc7732934f50a9095a450a22a4dfefd4fd06e
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-14 00:46:13 +0530

    add contributions function that renders contributors

---
+++ Frontend/src/Components/Contributions.jsx
@@ -0,0 +1,23 @@
+function Contributions({ people }) {
+  return (
+    <section className="py-12">
+      <h2 className="text-3xl sm:text-4xl text-center mb-8">Contributors</h2>
+      <div className="grid grid-cols-2 justify-items-center sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8 max-w-6xl mx-auto">
+        {people.map((person, index) => (
+          <a href={person.url}>
+            <div key={index} className="flex flex-col items-center">
+            <img
+              src={person.image}
+              alt={person.name}
+              className="w-17 h-17 rounded-full object-cover border-2 border-gray-300 shadow-sm"
+            />
+            <p className="text-[15px] mt-2">{person.name}</p>
+          </div>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Contributions;
