a01ef56a95daa16a5b7780e72f0cfeeb0bede70a
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-14 02:11:41 +0530

    add background image

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -3,14 +3,18 @@ import NavBar from '../Components/NavBar.jsx'
 import Hero from '../Components/Hero.jsx'
 import Contributors from '../Components/contributors.jsx'
 import SearchBar from '../Components/SearchBar.jsx'
+import backgroundImage from '../assets/bg.png'
 
 const HomePage = () => {
   return (
     <>
-        <NavBar />
-        <Hero />
-        <SearchBar />
-        <Contributors />
+      <NavBar />
+      <div className="min-h-screen bg-cover bg-center bg-no-repeat" 
+        style={{ backgroundImage: `url(${backgroundImage})` }}>
+          <Hero />
+          <SearchBar />
+          <Contributors />
+      </div>
     </>
   )
 }
