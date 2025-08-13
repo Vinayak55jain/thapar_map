f1d416d4d8d075afb29ad721d81c8f48bfc12a91
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-14 01:44:37 +0530

    made navbar responsive

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom'
 function NavBar() {
     return (
         <header className='bg-[#009bd5]/20 sticky top-0 z-10 shadow-md'>
-            <nav className="flex items-center justify-between h-28 max-w-7xl mx-auto px-6">
-                <Link href="/"><img src={logo} alt="ACM Thapar Logo" className="h-[102px] w-auto" /></Link>
-                <div className="flex items-center gap-4 h-14 w-32">
+            <nav className="flex items-center justify-between h-20 sm:h-28 max-w-7xl mx-auto px-6">
+                <Link href="/"><img src={logo} alt="ACM Thapar Logo" className="h-[70px] sm:h-[102px] w-auto" /></Link>
+                <div className="flex items-center gap-4 h-10 sm:h-14 w-32">
                     <a href="#"><img src={InstagramIcon} alt="Instagram" className="h-full" /></a>
                     <a href="#"><img src={GitHubIcon} alt="GitHub" className="h-full" /></a>
                 </div>
