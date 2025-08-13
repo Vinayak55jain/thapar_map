92be47a0cc196f7c3cae4781fbfc1e1e586c3e4f
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-13 23:59:42 +0530

    Add hero section

---
+++ Frontend/src/Components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <main className="relative flex flex-col items-center text-center py-16">
+        <h1 className="text-4xl sm:text-5xl mb-2">Interactive Thapar Map</h1>
+        <p className="text-2xl text-gray-700 mb-8">Thapar University</p>
+    </main>
+  )
+}
+
+export default Hero
