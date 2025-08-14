ad20db9e8557f1c9497a06fb81154d552001bace
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-14 12:31:18 +0530

    fix viewport height

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -7,15 +7,15 @@ import backgroundImage from '../assets/bg.png'
 
 const HomePage = () => {
   return (
-    <>
+    <div className="min-h-screen">
       <NavBar />
-      <div className="min-h-screen bg-cover bg-center bg-no-repeat" 
+      <div className="min-h-[calc(100vh-4rem)] sm:min-h-[calc(100vh-6rem)] bg-cover bg-center bg-no-repeat" 
         style={{ backgroundImage: `url(${backgroundImage})` }}>
           <Hero />
           <SearchBar />
           <Contributors />
       </div>
-    </>
+    </div>
   )
 }
 
