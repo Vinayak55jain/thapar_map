ed7ee7fc2b711d67249476476854b099ec95370e
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-14 17:41:26 +0530

    add temp search area div just to see how it looks

--- Frontend/src/Components/SearchBar.jsx
+++ Frontend/src/Components/SearchBar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <>
-    </>
+    <div className='flex justify-center'>
+      <div className='bg-[#009BD5]/80 rounded-[10px] border-white border-1 p-4 shadow-md'>
+        <div className='rounded-[10px] border-white border-2 w-[360px] h-[180px] sm:w-[630px] sm:h-[250px] shadow-md'>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
